884e0a3122fa7dc94c62ba54422fcf30f1509789
abdulhakeemidr <56049093+abdulhakeemidr@users.noreply.github.com>
2023-02-05 14:04:36 -0600

    Set Up Camera Controller with Cinemachine

--- TurnBasedStrategyCourse/Assets/Scripts/Grid/GridObject.cs
+++ TurnBasedStrategyCourse/Assets/Scripts/Grid/GridObject.cs
@@ -6,15 +6,38 @@ public class GridObject
 {
     private GridSystem gridSystem;
     private GridPosition gridPosition;
+    private List<Unit> unitListOnGrid;
 
     public GridObject(GridSystem gridSystem, GridPosition gridPosition)
     {
         this.gridSystem = gridSystem;
         this.gridPosition = gridPosition;
+        unitListOnGrid = new List<Unit>();
+    }
+
+    public List<Unit> GetUnitList()
+    {
+        return unitListOnGrid;
+    }
+
+    public void AddUnit(Unit unit)
+    {
+        unitListOnGrid.Add(unit);
+    }
+
+    public void RemoveUnit(Unit unit)
+    {
+        unitListOnGrid.Remove(unit);
     }
 
     public override string ToString()
     {
-        return gridPosition.ToString();
+        string unitString = "";
+        foreach(Unit unit in unitListOnGrid)
+        {
+            unitString += unit + "\n";
+        }
+
+        return gridPosition.ToString() + "\n" + unitString;
     }
 }
--- TurnBasedStrategyCourse/Assets/Scripts/Unit.cs
+++ TurnBasedStrategyCourse/Assets/Scripts/Unit.cs
@@ -9,10 +9,20 @@ public class Unit : MonoBehaviour
     private Animator unitAnimator;
 
     private Vector3 targetPosition;
+    private GridPosition gridPosition;
 
     private void Awake()
     {
         targetPosition = transform.position;
+    }
+
+    private void Start()
+    {
+        // Unit calculates its own position relative to the floor grid
+        gridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        // Unit calles this function which allows the gridObject the unit stands on to
+        // store a reference to the unit
+        LevelGrid.Instance.AddUnitToGridPosition(gridPosition, this);
     }
 
     private void Update() 
@@ -34,7 +44,14 @@ public class Unit : MonoBehaviour
             unitAnimator.SetBool("isWalking",false);
         }
 
-        
+        // Checks to see if the gridObject below the Unit has changed
+        // if gridPosition has changed, 
+        GridPosition newGridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        if(newGridPosition != gridPosition)
+        {
+            LevelGrid.Instance.UnitMovedGridPosition(this, gridPosition, newGridPosition);
+            gridPosition = newGridPosition;
+        }
     }
     public void Move(Vector3 targetPosition)
     {
